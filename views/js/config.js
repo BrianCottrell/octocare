cf150fd3bf3ad4c4b7781bae1057e27fc6cffef6
BrianCottrell <Brian_Cottrell@inbox.com>
2018-10-14 20:25:14 -0700

    Updated config

--- views/js/config.js
+++ views/js/config.js
@@ -10,6 +10,6 @@ var SAMPLE_SERVER_BASE_URL = 'https://opentok-web-samples-backend.herokuapp.com'
 // set these values to OpenTok API key, a valid session ID, and a token for the session.
 // For test purposes, you can obtain these from https://tokbox.com/account.
 
-var API_KEY = '';
-var SESSION_ID = '';
-var TOKEN = '';
+var API_KEY = '46204462';
+var SESSION_ID = '1_MX40NjIwNDQ2Mn5-MTUzOTU2NzQxNzg1Mn5ONTh1YmxMZkIwS2tTWlRmZk4zMkxuWjl-fg';
+var TOKEN = 'T1==cGFydG5lcl9pZD00NjIwNDQ2MiZzaWc9NzJjZGM4NDMzMTBlNzliZGE5Yzc2ZTg5ODlhMmNkODQ2ZGMyMDA5MTpzZXNzaW9uX2lkPTFfTVg0ME5qSXdORFEyTW41LU1UVXpPVFUyTnpReE56ZzFNbjVPTlRoMVlteE1aa0l3UzJ0VFdsUm1aazR6TWt4dVdqbC1mZyZjcmVhdGVfdGltZT0xNTM5NTY3NDQ0Jm5vbmNlPTAuMjIwMDg2NzU0NjI4OTM2MjMmcm9sZT1tb2RlcmF0b3ImZXhwaXJlX3RpbWU9MTU0MjE2MzA0MiZpbml0aWFsX2xheW91dF9jbGFzc19saXN0PQ==';
